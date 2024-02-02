91763f7976b2da5d64c5ce5f2bb8965c26697983
aleesha1999 <154547318+aleesha1999@users.noreply.github.com>
2024-02-02 22:37:11 +0530

    Add files via upload

--- studentsdetails.py
+++ studentsdetails.py
@@ -1,5 +1,5 @@
 class student:
-  def __int__(self,rollno,name, mark1, mark2):
+  def __init__(self,rollno,name, mark1, mark2):
     self.rollno=rollno
     self.name=name
     self.mark1=mark1
@@ -7,18 +7,17 @@ class student:
   def result(self):
     total= self.mark1+self.mark2
     return total
-    s=[]
+s=[]
 for i  in range(1,4):
-    roll=int(input("Enter rollno:"))
-    name=input("Enter name:")
-    m1=int(input("Enter mark1:"))
-    m2=int(input("Enter mark2:"))
-    #o=bank(a,n,t,bal=0)
-    obj=student(roll,name,m1,m2)
-    s.append(obj)
-t1=s[1].result()
-t1=s[2].result()
-t1=s3result()
+ roll=int(input("Enter rollno:"))
+ name=input("Enter name:")
+ m1=int(input("Enter mark1:"))
+ m2=int(input("Enter mark2:"))
+ obj=student(roll,name,m1,m2)
+ s.append(obj)
+t1=s[0].result()
+t2=s[1].result()
+t3=s[2].result()
 print("total mark of student1=",t1)
 print("total mark of student1=",t2)
 print("total mark of student1=",t3)
